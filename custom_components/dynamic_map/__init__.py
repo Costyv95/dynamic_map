d0ddbaa125dad29f3de28054b6ced5844f87163d
Systems Architect <architect@theseus.dev>
2026-05-11 11:24:57 +0200

    fix: bump editor version to v2.4 to bust cache

--- custom_components/dynamic_map/__init__.py
+++ custom_components/dynamic_map/__init__.py
@@ -36,7 +36,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
         sidebar_title="Map Editor",
         sidebar_icon="mdi:map-search-outline",
         frontend_url_path="dynamic_map_editor",
-        config={"url": "/dynamic_map_ui/editor.html?v=2.2"},
+        config={"url": "/dynamic_map_ui/editor.html?v=2.4"},
         require_admin=True,
     )
     
