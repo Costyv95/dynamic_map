0de07885857b25a268381bd13f1ad61a7bb09f23
Systems Architect <architect@theseus.dev>
2026-05-17 14:27:49 +0200

    Revert "feat(editor): Allow room splitting using primary left-click and touch drag"
    This reverts commit 040f8e33f05e8fe6efaf9153643596f7ab7d4903.

--- custom_components/dynamic_map/frontend/editor/EditorInteractionManager.js
+++ custom_components/dynamic_map/frontend/editor/EditorInteractionManager.js
@@ -130,13 +130,7 @@ export class EditorInteractionManager {
             return;
         }
 
-        if (this.state.isEditMode && this.state.selectedRooms.length === 1) {
-            this.interactionState = 'MAYBE_SPLIT';
-            this.state.splitStart = this.getMousePos(e);
-            this.state.isSplitting = false;
-        } else {
-            this.interactionState = 'MAYBE_PAN';
-        }
+        this.interactionState = 'MAYBE_PAN';
     }
 
     onPointerMove(e) {
@@ -260,19 +254,6 @@ export class EditorInteractionManager {
             return;
         }
 
-        if (this.interactionState === 'MAYBE_SPLIT') {
-            let clientX = e.clientX;
-            let clientY = e.clientY;
-            if (e.touches && e.touches.length > 0) {
-                clientX = e.touches[0].clientX;
-                clientY = e.touches[0].clientY;
-            }
-            const dist = Math.hypot(clientX - this.panStart.x, clientY - this.panStart.y);
-            if (dist > 5) {
-                this.interactionState = 'SPLIT';
-            }
-        }
-
         if (this.interactionState === 'MAYBE_PAN') {
             let clientX = e.clientX;
             let clientY = e.clientY;
@@ -328,11 +309,6 @@ export class EditorInteractionManager {
                 this.state.saveState();
                 this.state.updateUICallback();
             }
-        }
-
-        if (this.interactionState === 'MAYBE_SPLIT') {
-            this.interactionState = 'MAYBE_PAN';
-            this.isDragging = false;
         }
 
         if (this.interactionState === 'MAYBE_PAN' && !this.isDragging) {
