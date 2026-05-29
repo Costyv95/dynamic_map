1c9eab9ba86292e315c58b0074188f0ee3715cd8
Systems Architect <architect@theseus.dev>
2026-05-29 22:33:08 +0200

    Increment version to 2.71 to bust browser cache on visual editor modules

--- custom_components/dynamic_map/frontend/editor.js
+++ custom_components/dynamic_map/frontend/editor.js
@@ -1,8 +1,8 @@
-import { ApiManager } from './shared/ApiManager.js?v=2.70';
-import { CanvasEngine } from './editor/CanvasEngine.js?v=2.70';
-import { EditorStateManager } from './editor/EditorStateManager.js?v=2.70';
-import { EditorInteractionManager } from './editor/EditorInteractionManager.js?v=2.70';
-import { EditorUIManager } from './editor/EditorUIManager.js?v=2.70';
+import { ApiManager } from './shared/ApiManager.js?v=2.71';
+import { CanvasEngine } from './editor/CanvasEngine.js?v=2.71';
+import { EditorStateManager } from './editor/EditorStateManager.js?v=2.71';
+import { EditorInteractionManager } from './editor/EditorInteractionManager.js?v=2.71';
+import { EditorUIManager } from './editor/EditorUIManager.js?v=2.71';
 
 console.log('[DynamicMapDebug] Active Map Editor Loaded (Version: 2.70)');
 
